b43332e576e24ede54a4f4e8bb1fcee619f4efa8
Arthur GROSSI <82841205+aarrtH8@users.noreply.github.com>
2026-03-31 09:13:20 +0200

    Delete netbox_device_cleaner directory  to create new repo

--- netbox_device_cleaner/netbox_device_cleaner/navigation.py
+++
@@ -1,8 +0,0 @@
-from netbox.plugins import PluginMenuItem
-
-menu_items = (
-    PluginMenuItem(
-        link='plugins:netbox_device_cleaner:purge',
-        link_text='Device Cleaner',
-    ),
-)
--- netbox_device_cleaner/netbox_device_cleaner/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'netbox_device_cleaner'
-
-urlpatterns = [
-    path('', views.PurgeView.as_view(), name='purge'),
-]
